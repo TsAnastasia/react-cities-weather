f3bfec846b91ee45054405be76a7b65d8478d50f
TsAnastasia <anastasiacibernaja@yandex.ru>
2022-01-12 17:46:17 +0300

    feat: create footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Search from "./components/Search/Search";
 import Weather from "./components/Weather/Weather";
 import { useAppSelector } from "./hooks/redux";
 import styles from "./app.module.scss";
+import Header from "./components/Header/Header";
 
 const App = () => {
   const { city } = useAppSelector((state) => state.cityReducer);
   return (
     <div className={styles.app}>
-      <header>header</header>
+      <Header />
       <main className={styles.main}>
         <Intro />
         <section>
--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,8 +3,8 @@ import styles from "./intro.module.scss";
 
 const Intro = () => (
   <section className={styles.intro}>
-    <h1>City's weather app</h1>
-    <p>Find out weather in your city</p>
+    <h1 className={styles.title}>City's weather app</h1>
+    <p className={styles.description}>Find out weather in your city</p>
   </section>
 );
 
